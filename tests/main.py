d4e72c3183dbbe2f4492b463ece2bf545e5f3e3e
David <davidvillegasr@gmail.com>
2025-03-12 06:07:47 -0400

    verifying existance of download buttons

--- tests/main.py
+++ tests/main.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.by import By
 
 from drivers.driver_manager import Browser
 from pages.ielts_home import IeltsHome
@@ -14,11 +15,17 @@ driver = browser.get_driver()
 ielts_home = IeltsHome(driver)
 ielts_home.open_ielts_home()
 home_title = ielts_home.get_title()
+
+##### Accepting cookies #####
 ielts_home.accept_cookies()
 assert len(driver.window_handles) == 1
+
+##### Entering the downloads page #####
 ielts_home.click_on_login_link()
 ielts_home.wait_second_tab()
 ielts_home.switch_to_new_tab()
+
+##### Entering credentials #####
 ielts_results = IeltsResultsService(driver)
 print(driver.title)
 ielts_results.insert_email()
@@ -26,12 +33,22 @@ ielts_results.insert_password()
 ielts_results.click_on_login_button()
 print(driver.current_url)
 print(driver.title)
+
+##### Entering the Download page for the second time #####
 ielts_results.click_on_download_results()
 print(driver.current_url)
 print(driver.title)
-results_title = ielts_results.get_title()
 
-
+download_new_results = driver.find_elements(By.XPATH, "//div[@class='newresult']//a[contains(@data-action, 'downloadNewCandidates')]")
+if download_new_results:
+    print("element present")
+else:
+    print("element absent")
+download_updated_results = driver.find_elements(By.XPATH, "//div[@class='updatedresult']//a[contains(@data-action, 'downloadNewCandidates')]")
+if download_updated_results:
+    print("element present")
+else:
+    print("element absent")
 ##### printing the title's page to name the files
 #second_page =ielts_home.get_title()
 #titulo = driver.title
